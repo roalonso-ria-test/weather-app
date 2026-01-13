cd07a0f7b052c4f65bd8db2018538bf00e8bdfdb
Rodrigo Alonso <rodrigo.alonso.o+ria-test@gmail.com>
2026-01-13 19:47:11 -0300

    Use tanstack query to handle request/caching status

--- src/api/composables/useForecast.ts
+++ src/api/composables/useForecast.ts
@@ -1,8 +1,21 @@
-export function useForecast(lat: number, lon: number) {
-  const params = new URLSearchParams({
-    appid: import.meta.env.VITE_OPENWEATHER_API_KEY,
-    lat: lat.toString(),
-    lon: lon.toString(),
+import { useQuery } from "@tanstack/vue-query";
+import { toValue, type MaybeRef } from "vue";
+
+export function useForecast(lat: MaybeRef<number>, lon: MaybeRef<number>) {
+  return useQuery({
+    queryKey: ["forecast", lat, lon],
+    queryFn: async () => {
+      const params = new URLSearchParams({
+        appid: import.meta.env.VITE_OPENWEATHER_API_KEY,
+        lat: toValue(lat).toString(),
+        lon: toValue(lon).toString(),
+      });
+      const url = `https://api.openweathermap.org/data/2.5/weather?${params}`;
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    },
   });
-  const url = `https://api.openweathermap.org/data/2.5/weather?${params}`;
 }
--- src/api/composables/useGeocoordinates.ts
+++ src/api/composables/useGeocoordinates.ts
@@ -1,8 +1,21 @@
-export function useGeocoordinates(q: string) {
-  const params = new URLSearchParams({
-    appid: import.meta.env.VITE_OPENWEATHER_API_KEY,
-    q,
-    limit: "1",
+import { useQuery } from "@tanstack/vue-query";
+import { toValue, type MaybeRef } from "vue";
+
+export function useGeocoordinates(q: MaybeRef<string>) {
+  return useQuery({
+    queryKey: ["geocoordinates", q],
+    queryFn: async () => {
+      const params = new URLSearchParams({
+        appid: import.meta.env.VITE_OPENWEATHER_API_KEY,
+        q: toValue(q),
+        limit: "1",
+      });
+      const url = `http://api.openweathermap.org/geo/1.0/direct?${params}`;
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    },
   });
-  const url = `http://api.openweathermap.org/geo/1.0/direct?${params}`;
 }
